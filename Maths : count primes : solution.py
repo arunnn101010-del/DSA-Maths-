f9eb43d608407d87367a5190c32e1862b6864c71
Arun <arunnn101010@gmail.com>
2026-05-21 16:53:49 +0530

    Create Maths : count primes : solution.py

---
+++ Maths : count primes : solution.py
@@ -0,0 +1,31 @@
+# Promblem - count primes
+# Time and space complexity - 0(n log log n) & 0(n)
+# Leetcode and diffculty level - 204 & medium 
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n <= 2) {
+            return 0;
+        }
+        vector<bool> prime(n, true);
+
+        prime[0] = false;
+        prime[1] = false;
+
+        for(int i=2; i*i < n; i++) {
+            if(prime[i]) {
+                for(int j=i*i; j < n; j+=i) {
+                    prime[j] = false;
+                } 
+            }
+        }
+        int count = 0;
+
+        for(int i=2; i<n; i++) {
+            if(prime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
