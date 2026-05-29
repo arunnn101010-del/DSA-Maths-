90174d565bd1ec91d620dba45c62d9ac354067a8
Arun <arunnn101010@gmail.com>
2026-05-29 22:50:28 +0530

    Update Maths : valid number : solution.py

--- Maths : valid number : solution.py
+++ Maths : valid number : solution.py
@@ -1,1 +1,39 @@
+# Promblem - valid number 
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and diffculty level - 65 & hard
+class Solution {
+public:
+    bool isNumber(string s) {
+        bool digit = false;
+        bool dot = false;
+        bool exp = false;
 
+        for(int i=0; i<s.size(); i++) {
+            if(isdigit(s[i])) {
+                digit = true;
+            }
+            else if(s[i] == '+' || s[i] =='-') {
+                if(i > 0 && s[i-1] != 'e' && s[i-1] != 'E') {
+                    return false;
+                }
+            }
+            else if(s[i] == '.') {
+                if(dot || exp) {
+                    return false;
+                }
+                dot = true;
+            }
+            else if(s[i] == 'e' || s[i] == 'E') {
+                if(exp || !digit) {
+                    return false;
+                }
+                exp = true;
+                digit = false;
+            }
+            else {
+                return false;
+            }
+        }
+        return digit;
+    }
+};
